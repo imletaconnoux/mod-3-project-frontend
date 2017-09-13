c4b219d16e0a973718731f17bd595f51b6a89d10
github username <github email address>
2017-09-13 09:45:28 -0400

    started search function

--- src/components/products.js
+++ src/components/products.js
@@ -1,18 +1,20 @@
 class Products {
   constructor() {
     this.products = [] // this refers to all of the product objects
-    // this.initBindingsAndEventListeners()
+    this.initBindingsAndEventListeners()
     this.adapter = new ProductsAdapter()
     this.fetchAndLoadProducts() // refer to line 17
   }
 
-  // initBindingsAndEventListeners() {
-  //   // this.productsForm = document.getElementById('new-product-form')
-  //   // this.productInput = document.getElementById('new-product-body')
-  //   // this.productsNode = document.getElementById('products-container')
-  //   // this.productsForm.addEventListener('submit',this.handleAddProduct.bind(this))
-  //   // this.productsNode.addEventListener('click',this.handleDeleteProduct.bind(this))
-  // }
+  initBindingsAndEventListeners() {
+    this.searchInput = document.getElementById("find-product-input")
+    this.searchInput.addEventListener("keyup", function(event){console.log(event.target.value)})
+    // this.productsForm = document.getElementById('new-product-form')
+    // this.productInput = document.getElementById('new-product-body')
+    // this.productsNode = document.getElementById('products-container')
+    // this.productsForm.addEventListener('submit',this.handleAddProduct.bind(this))
+    // this.productsNode.addEventListener('click',this.handleDeleteProduct.bind(this))
+  }
 
 // this will hit your database and load all of your products
   fetchAndLoadProducts() {
@@ -24,14 +26,21 @@ class Products {
       .catch( (e) => console.log(e) )
   }
 
-  handleAddProduct() {
+  handleSearch() {
     event.preventDefault()
-    const body = this.productInput.value
-    this.adapter.createProduct(body)
-    .then( (productJSON) => this.products.push(new Product(productJSON)) )
-    .then(  this.render.bind(this) )
-    .then( () => this.productInput.value = '' )
+    let searchTerm = event.target.value
+    console.log(this.findProducts(searchTerm)) // define on class
+
   }
+
+  // handleAddProduct() {
+  //   event.preventDefault()
+  //   const body = this.productInput.value
+  //   this.adapter.createProduct(body)
+  //   .then( (productJSON) => this.products.push(new Product(productJSON)) )
+  //   .then(  this.render.bind(this) )
+  //   .then( () => this.productInput.value = '' )
+  // }
 
   handleDeleteProduct() {
     if (event.target.dataset.action === 'delete-product' && event.target.parentElement.classList.contains("product-element")) {
@@ -53,4 +62,13 @@ class Products {
   render() {
     this.productsNode.innerHTML = `<ul>${this.productsHTML()}</ul>`
   }
+
+  findProducts(searchTerm){
+    this.products.filter((searchTerm) => {
+      if (foundProducts = product.name.includes(searchTerm)) {
+        return product
+      }
+    })
+  }
+
 }
